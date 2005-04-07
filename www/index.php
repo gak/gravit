8bc92e25e7e1f5edcb90c967efa6efe13937d2ab
gakman <gakman@60970b36-f649-e149-802f-859a9d0a703e>
2005-04-07 01:25:29 +0000

    gravit.cfg: videobpp should be 32 by default www updates (HTML/CSS VALID!)
    git-svn-id: svn://gakman.com/gravit/trunk@66 60970b36-f649-e149-802f-859a9d0a703e

--- www/index.php
+++ www/index.php
@@ -1,8 +1,13 @@
+<? header("Content-type: text/html; charset=iso-8859-1"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
 <html>
+
+<head>
 
 <title>Gravit - A gravity simulator</title>
 
-<style>
+<style type="text/css">
 
 body { background-color: black; color: white; font-family: verdana; font-size: 11px; }
 .darkertext { color: #A0A0A0 }
@@ -11,14 +16,22 @@ a:hover { color: red; text-decoration: underline; }
 .darkertext { color: #A0A0A0; }
 .normtext { color: #E0E0E0; }
 
+img { border: none; }
+
 </style>
+
+<link rel="shortcut icon" href="favicon.ico">
+<meta name="resource-type" content="document">
+<meta name="description" content="Gravit is a gravity simulator that uses OpenGL and SDL">
+<meta name="keywords" content="Gravit, Graviton, gravity, nbody, N-body, N body, simulate, simulation, simulator, octree, octtree, OpenGL, SDL, GL_ARB_point_parameters, GL_ARB_point_sprite">
+</head>
 
 <body>
 
 <h1>Gravit</h1>
 
 <h2>About</h2>
-Gravit is a gravity simulator which runs under Linux and Windows. It's released under the <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a> which makes it free. It uses simple newtonian physics using the Barnes-Hut N-body algorithm. Although the main goal of Gravit is to be as accurate as possible, it also creates beautiful looking gravity patterns. It records the history of each particle so it can animate and display a path of its travels. At any stage you can rotate your view in 3D and zoom in and out. Gravit uses OpenGL with <a href="http://www.libsdl.org/">SDL</a>, <a href="http://www.libsdl.org/projects/SDL_ttf/">SDL_ttf</a> and <a href="http://www.libsdl.org/projects/SDL_image/">SDL_image</a>.
+<p>Gravit is a gravity simulator which runs under Linux and Windows. It's released under the <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a> which makes it free. It uses simple newtonian physics using the Barnes-Hut N-body algorithm. Although the main goal of Gravit is to be as accurate as possible, it also creates beautiful looking gravity patterns. It records the history of each particle so it can animate and display a path of its travels. At any stage you can rotate your view in 3D and zoom in and out. Gravit uses OpenGL with <a href="http://www.libsdl.org/">SDL</a>, <a href="http://www.libsdl.org/projects/SDL_ttf/">SDL_ttf</a> and <a href="http://www.libsdl.org/projects/SDL_image/">SDL_image</a>.</p>
 
 <h2>Screen Shots</h2>
 
@@ -30,6 +43,7 @@ $cols = 4;
 $colcount = 0;
 
 echo '<table>';
+echo '<tr>';
 
 for ($i = $from; $i <= $to; $i++) {
 
@@ -45,7 +59,7 @@ for ($i = $from; $i <= $to; $i++) {
 	
 	echo '<td>';
 	echo '<a href="screenies/big/' . $fn . '.jpg">';
-	echo '<img border="0" width="200" height="160" src="screenies/small/' . $fn . '.jpg">';
+	echo '<img width="200" height="160" src="screenies/small/' . $fn . '.jpg" alt="Gravit Screenshot">';
 	echo '</a>';
 
 }
@@ -56,12 +70,15 @@ echo '</table>';
 
 <h2>Demonstration Videos</h2>
 
-<a href="video/gravit-001.avi">Zoomed in on a galaxy showing off v0.2 effects</a> - 9s, 1.1MB, 800x600<br>
-<a href="video/gravit-002.avi">Many galaxies showing off v0.2 effects and the frameskip command</a> - 30s, 3.6MB, 1280x1024<br>
-<a href="video/gravit-003.avi">Another v0.2 demo, draws an OctTree</a> - 36s, 4.0MB, 800x600<br>
-<a href="video/gravit-004.avi">You must get this one!</a> - 22s, 2.5MB, 800x600<br>
+<ul>
+<li><a href="video/gravit-001.avi">Zoomed in on a galaxy showing off v0.2 effects</a> - 9s, 1.1MB, 800x600</li>
+<li><a href="video/gravit-002.avi">Many galaxies showing off v0.2 effects and the frameskip command</a> - 30s, 3.6MB, 1280x1024</li>
+<li><a href="video/gravit-003.avi">Another v0.2 demo, draws an OctTree</a> - 36s, 4.0MB, 800x600</li>
+<li><a href="video/gravit-004.avi">You must get this one!</a> - 22s, 2.5MB, 800x600</li>
+</ul>
 
 <h2>Features</h2>
+<ul>
 <li>It looks pretty :)</li>
 <li>You can record, then play back at any speed</li>
 <li>Stores every particle's position for every frame in memory</li>
@@ -69,27 +86,34 @@ echo '</table>';
 <li>Mouse controllable rotation</li>
 <li>Console with script execution</li>
 <li>Colours can be based on mass, velocity or acceleration</li>
+</ul>
 
 <h2>Details</h2>
+<ul>
 <li>So far Gravit has been tested on Windows XP and Linux Slackware.</li>
 <li>It is written in C and uses SDL, SDL_ttf, SDL_image and OpenGL.</li>
 <li>It uses the <a href="http://www.amara.com/papers/nbody.html#tcu">Barnes-Hut Algorithm</a> (with room for optimisation).</li>
 <li>Gravit was called Graviton, but i changed the name because someone already used Graviton for their project!</li>
+</ul>
 
 <h2>Download</h2>
 
+<p>
 <!-- If you're unsure of things, get the stable version otherwise the development release shouldn't be too bad!<br> -->
 Read the <a href="dist/ChangeLog">ChangeLog</a> for the latest release details.<br>
+</p>
 
 <?
 
 function showFile($file, $desc) {
 
-	echo "<a href=\"dist/$file\">$file</a> $desc (".number_format(filesize("dist/$file"))." bytes)<br>";
+	echo "<li><a href=\"dist/$file\">$file</a> $desc (".number_format(filesize("dist/$file"))." bytes)</li>";
 
 }
 
 function showFiles($ver) {
+
+	echo '<ul>';
 
 	if ($ver == "0.1")
 		showFile("gravit-$ver-win32-sdl.zip", "Gravit $ver for Windows with SDL and SDL_ttf");
@@ -97,6 +121,8 @@ function showFiles($ver) {
 		showFile("gravit-$ver-win32-dll.zip", "Gravit $ver for Windows with SDL, SDL_ttf, SDL_image");
 	showFile("gravit-$ver-win32.zip", "Gravit $ver for Windows");
 	showFile("gravit-$ver-src.tgz", "Gravit $ver Source Code");
+	
+	echo '</ul>';
 	
 }
 
@@ -116,6 +142,7 @@ showFiles("0.1");
 ?>
 
 <h2>Some Features To Do</h2>
+<ul>
 <li>Fix gimbal lock with 3d rotation</li>
 <li>Better camera controls</li>
 <li>Console tab-completition</li>
@@ -131,6 +158,8 @@ showFiles("0.1");
 <li>Command line parameters</li>
 <li>Multi-threading</li>
 <li>Screensaver</li>
+</ul>
+
 </body>
 
 </html>
