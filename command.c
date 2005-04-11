da821bd6854c3a795deeb5d2753eb9d225564e36
gakman <gakman@60970b36-f649-e149-802f-859a9d0a703e>
2005-04-11 01:23:39 +0000

    fixed G bug from changeset:117
    git-svn-id: svn://gakman.com/gravit/trunk@124 60970b36-f649-e149-802f-859a9d0a703e

--- command.c
+++ command.c
@@ -756,12 +756,15 @@ void cmdSetG(char *arg) {
 	float newg;
 
 	newg = -pow(10, -state.gbase);
+	conAdd(0, "\"G\" set to %f", newg);
 
 	if (newg != state.g) {
 
 		conAdd(2, "Warning! Changing the gravitantional constant while recording may cause fundimental problems! :)");
 
 	}
+
+	state.g = newg;  
 	
 }
 
