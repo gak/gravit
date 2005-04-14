93bcb8e6078b9bdb7b5eb705b6fe56f2b3659bfc
gakman <gakman@60970b36-f649-e149-802f-859a9d0a703e>
2005-04-14 11:04:46 +0000

    documentation updates
    git-svn-id: svn://gakman.com/gravit/trunk@171 60970b36-f649-e149-802f-859a9d0a703e

---
+++ www/docs/Index.php
@@ -0,0 +1,28 @@
+<? header("Content-type: text/html; charset=iso-8859-1"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>Gravit Documentation - Index</title>
+<style type="text/css">
+@import url(../gravit.css);
+</style>
+</head>
+<body>
+<h1>Gravit Documentation</h1>
+<p>
+<a class="ext-link" title="http:.." href="http:..">Gravit Home</a>
+</p>
+<h2>Quick Introduction</h2>
+<p>
+Here's the documentation for Gravit. I will try to maintain it along with new features of Gravit. I hope it is useful!
+</p>
+<h2>Index</h2>
+<ol><li>Overview (TODO)
+</li><li><a href="Console.php">Console Commands</a>
+</li><li>Recording and Playback (TODO)
+</li><li><a href="StereoMode.php">Stereoscopic Mode</a>
+</li><li>Examples (TODO)
+</li><li>Troubleshooting (TODO)
+</li></ol>
+</body>
+</html>
---
+++ www/docs/StereoMode.php
@@ -0,0 +1,53 @@
+<? header("Content-type: text/html; charset=iso-8859-1"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>Gravit Documentation - StereoMode</title>
+<style type="text/css">
+@import url(../gravit.css);
+</style>
+</head>
+<body>
+<h1>Stereoscopic Mode</h1>
+<p>
+<a class="ext-link" title="http:.." href="http:..">Gravit Home</a> / <a href="Index.php">Documentation Index</a>
+</p>
+<h2>Overview</h2>
+<p>
+Stereoscopy is a way to see a 3D image on a 2D screen. It is similar to the way the <a class="ext-link" title="http://www.magiceye.com/" href="http://www.magiceye.com/">Magic Eye</a> books do it.
+</p>
+<p>
+There are two ways Gravit can display stereoscopic images. They are cross-eyed and parallel viewing. I'm not an expert in stereoscopy, so please don't take my word for this -- all I know is that it works!
+</p>
+<p>
+To get cross-eyed viewing set up in Gravit, you'll need to get into the console using the ` key and typing in the following:
+</p>
+<pre class="wiki"> stereo 1
+ stereoseparation -100
+</pre><p>
+The <i>stereoseparation</i> value is adjustable, the more negative this number is, the more 'depth' you will see.
+</p>
+<p>
+To get parallel viewing going, the <i>stereoseparation</i> variable is positive:
+</p>
+<pre class="wiki"> stereo 1
+ stereoseparation 100
+</pre><p>
+Increasing <i>stereoseparation</i> to a larger number will increse depth.
+</p>
+<p>
+For more information on how to get your eyes to do all this, check out the following links:
+</p>
+<ul><li><a class="ext-link" title="http://www.angelfire.com/ca/erker/freeview.html" href="http://www.angelfire.com/ca/erker/freeview.html">http://www.angelfire.com/ca/erker/freeview.html</a>
+</li><li><a class="ext-link" title="http://en.wikipedia.org/wiki/Stereoscopy" href="http://en.wikipedia.org/wiki/Stereoscopy">http://en.wikipedia.org/wiki/Stereoscopy</a>
+</li></ul><h2>Example Images</h2>
+<p>
+Below are two examples of each method. The larger orange coloured galaxy should be infront. 
+</p>
+<h3>Cross-eyed</h3>
+<div style="text-align: center"><img alt="Cross-eyed viewing" src="images/stereo-cross-eyed.jpg"></div>
+<h3>Parallel</h3>
+<div style="text-align: center"><img alt="Parallel viewing" src="images/stereo-parallel.jpg"></div>
+
+</body>
+</html>
