c4844abb8534890eaef171f0ab06f544d0199c0e
gak <gak@60970b36-f649-e149-802f-859a9d0a703e>
2005-05-29 10:58:07 +0000

    git-svn-id: svn://gakman.com/gravit/trunk@273 60970b36-f649-e149-802f-859a9d0a703e

--- spawn/default.lua
+++
@@ -1,8 +0,0 @@
-
-scripts = { "one-galaxy", "binary-galaxy", "many-galaxy", "snake", "loops" }
-n = table.getn(scripts)
-r = math.random(1,n)
-log("Spawining " .. scripts[r])
-spawn = nil
-require ("scripts/" .. scripts[r] .. ".lua")
-
