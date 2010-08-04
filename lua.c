d39c39d29ad12537005276fec7138c97cef7ec90
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-08-05 09:08:20 +1000

    define for lua.c incase it is included without lua

--- lua.c
+++ lua.c
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "gravit.h"
 
+#if HAVE_LUA
+
 int luaInit() {
 
     luaFree();
@@ -139,3 +141,6 @@ int luag_log(lua_State *L) {
     return 0;
 
 }
+
+#endif
+
