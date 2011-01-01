56956839896078ae5028b72fce214a6e8f40924f
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2011-01-02 09:05:38 +1100

    create installer script

---
+++ Xcode/Gravit/create-installer.sh
@@ -0,0 +1,4 @@
+#productbuild --component build/Release/Gravit.app /Applications --sign "3rd Party Mac Developer Installer: Gerald Kaszuba" --product Gravit-Info.plist Gravit.pkg
+
+productbuild --component build/Release/Gravit.app /Applications --sign "3rd Party Mac Developer Installer: Gerald Kaszuba" Gravit.pkg
+
