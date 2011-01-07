ccc7aca61111cb5406a2c83f546999ab93dc2198
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2011-01-08 09:59:18 +1100

    removed comment

--- Xcode/Gravit/create-installer.sh
+++ Xcode/Gravit/create-installer.sh
@@ -1,4 +1,1 @@
-#productbuild --component build/Release/Gravit.app /Applications --sign "3rd Party Mac Developer Installer: Gerald Kaszuba" --product Gravit-Info.plist Gravit.pkg
-
 productbuild --component build/Release/Gravit.app /Applications --sign "3rd Party Mac Developer Installer: Gerald Kaszuba" Gravit.pkg
-
