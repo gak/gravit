1ffc0b46e428c44f5a7ffdf62218b65846d210a6
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2012-01-08 08:34:17 +1100

    Store the SDL_Surface (for future use with a UI)

--- src/gravit.h
+++ src/gravit.h
@@ -339,7 +339,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
         char fontFile[MAX_FONT_LENGTH];
         int fontSize;
 
-        SDL_VideoInfo* gfxInfo;
+        SDL_VideoInfo *gfxInfo;
+        SDL_Surface *sdlScreen;
 
         int sdlStarted;
 
