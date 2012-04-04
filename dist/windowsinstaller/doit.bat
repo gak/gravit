b25698f75ebaae5d3f64507e8afa06225dddec90
FrMo <frank.moehle@btc-ag.com>
2012-04-04 21:02:17 +0200

    initial 32bit/64bit install scripts

--- dist/windowsinstaller/doit.bat
+++ dist/windowsinstaller/doit.bat
@@ -1,1 +1,3 @@
-"C:\Program Files\Inno Setup 5\compil32.exe" /cc d:\src\gravit\windowsinstaller\gravit-inno.iss
+"C:\Program Files\Inno Setup 5\compil32.exe" /cc d:\src\gravit\windowsinstaller\gravit-32bit-inno.iss
+
+"C:\Program Files\Inno Setup 5\compil32.exe" /cc d:\src\gravit\windowsinstaller\gravit-64bit-inno.iss
