aa7e7ca61ad25914ad8dd781e60b8983b32cb3be
FrMo <frank.moehle@btc-ag.com>
2012-04-08 18:10:10 +0200

    gravit could not find spawn scripts after install on linux

--- src/lua.c
+++ src/lua.c
@@ -141,7 +141,8 @@ int luag_load(lua_State *L) {
     conAdd(LLOW, s);
     lua_pop(L, 1);
     
-    s = va("spawn/%s", s);
+    //s = va("spawn/%s", s);
+    s = va("%s/%s", SPAWNDIR, s);
     
     f = findFile(s);
     conAdd(LLOW, f);
