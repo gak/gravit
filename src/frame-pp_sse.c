5008019c26df9f466510b70512bde84e96de40ac
FrMo <frmoehle@googlemail.com>
2014-05-09 17:17:23 +0200

    (debian licensecheck) fixed incorrect FSF address, added missing license headings

--- src/frame-pp_sse.c
+++ src/frame-pp_sse.c
@@ -1,7 +1,9 @@
 /*
 
+This file is part of
 Gravit - A gravity simulator
 Copyright 2003-2005 Gerald Kaszuba
+Copyright 2012 Frank Moehle
 
 Gravit is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,7 +17,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with Gravit; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 */
 
