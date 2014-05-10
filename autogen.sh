5c7a13c7b822eeacc1f1c52a9d030f3b56827ba7
FrMo <frmoehle@googlemail.com>
2014-05-10 18:54:00 +0200

    minor change to improve GNU standards compliance

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# The GNU standard wants to see some extra file in the top-level directory
+# (and if not found automake may report an error)
+touch AUTHORS NEWS README ChangeLog
+
+# build configure script
 aclocal -I acinclude
 autoheader
 automake --foreign --include-deps --add-missing --copy
